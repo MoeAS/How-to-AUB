3992198e1f81ffb22f49f3cf97bd2b8dd6e422a0
Mohamad Abou Salem <mba26@mail.aub.edu>
2022-04-17 19:45:30 +0300

    shizoww

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Mohammed\Desktop\Project\backend\venv
+set VIRTUAL_ENV=C:\Users\Mohammed\Desktop\How-to-AUB\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
