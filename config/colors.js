fb3777e6ab2e6b7bb767f300080cc3bae5d8728a
dinaYounes <dinayounes2000@gmail.com>
2022-05-04 02:25:28 +0300

    profile

---
+++ config/colors.js
@@ -0,0 +1,15 @@
+export default {
+    primary: "#fc5c65",
+    secondary: "#4ecdc4",
+    black: "#000",
+    white: "#fff",
+    gray: '#6e6969',
+    danger: '#ff5252',
+    yellow: '#ffd700',
+    whitesmoke: '#f5f5f5',
+    darkblue: '#2C3E4F',
+    steelblue: '#4682b4',
+    lightsteelblue: '#b0c4de',
+    lightslategrey: '#778899',
+    darkgray: '#23313E',
+}
